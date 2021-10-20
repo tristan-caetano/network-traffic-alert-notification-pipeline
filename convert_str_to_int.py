f89d6902592d0c48aa30a098168debd1fbf4a865
Jason Pinto <pintojason@outlook.com>
2021-10-20 18:55:30 -0400

    Created new file for datatype conversion

---
+++ convert_str_to_int.py
@@ -0,0 +1,10 @@
+import csv
+import pandas as pd
+import xlwt 
+from pandas.core.arrays.sparse import dtype
+from xlwt import Workbook
+
+def change_data_type():
+    # Start working here
+
+change_data_type()
