ebe74631593e7d908979c8e72f024fb8cfe93823
Tristan Caetano <tristancostacaetano@gmail.com>
2021-10-19 21:30:14 -0400

    Completed creating test set for alg

--- create_test_set.py
+++ create_test_set.py
@@ -8,8 +8,9 @@ from xlwt import Workbook
 # THIS SHOULD ONLY HAVE TO BE RUN ONCE PER FILE
 def digest_file():
     
+    print('\nOpening original dataset, please wait.')
     # Pandas Import CSV
-    p_dataset = pd.read_csv('UNSW-NB15_1.csv')
+    p_dataset = pd.read_csv('UNSW-NB15_1.csv', low_memory=False)
 
     # Creating dataset from CSV data
     dataframe = pd.DataFrame(p_dataset)
@@ -19,17 +20,15 @@ def digest_file():
     usable_rows = []
 
     # Prints number and names of unique values in malicious packet index
-    unique_vals = p_dataset[p_dataset.columns[47]].nunique()
+    #unique_vals = p_dataset[p_dataset.columns[47]].nunique()
     unique_names = p_dataset[p_dataset.columns[47]].unique()
-    print(unique_vals)
-    print(unique_names)
 
     # Creating a null value for the comparison
     null = p_dataset[p_dataset.columns[47]][0]
 
+    print('\nIterating through entire dataset, this may take a bit.')
     # For loops that populate the respective arrays
     for x in range(700000):
-        print(x)
         for y in range(10):
             if p_dataset[p_dataset.columns[47]][x] == unique_names[y] and p_dataset[p_dataset.columns[47]][x] != null:
                 arr[y] += 1
@@ -41,11 +40,17 @@ def digest_file():
         f.write(str(rows) + "\n")
     f.close()
 
-    # printing array for verification
-    print(arr)
+    print('\nFile Created: list_of_rows.txt')
 
+    make_test_file()
+
+# Creating CSV test set for Multilayer Perceptron
 def make_test_file():
+
+    # Opening original test set for data extraction
     r = open('list_of_rows.txt', 'r')
+
+    # Taking idicies of malicious packets from text file and placing them in a list variable
     line = r.readlines()
     new_line = []
     for rows in line:
@@ -53,7 +58,8 @@ def make_test_file():
         temp = str(temp).strip("['']")
         new_line.append(int(temp))
 
-    # Pandas Import CSV
+    print('\nOpening original dataset, please wait.')
+    # Pandas Import Original CSV
     p_dataset = pd.read_csv('UNSW-NB15_1.csv', low_memory = False)
 
     # Creating a null value for the comparison
@@ -61,23 +67,50 @@ def make_test_file():
 
     # Prints number and names of unique values in malicious packet index
     unique_names = p_dataset[p_dataset.columns[47]].unique()
-
+ 
+    # Creating workbook to save test set until its written to a csv
     wb = Workbook() 
     sheet1 = wb.add_sheet('Sheet 1') 
+
+    # Declaring variables for trackig packet count and tracking written rows
     counter = 0
     a = 0
 
-    for y in range(10):
+    print('\nSaving list of malware packets, please wait.')
+
+    # For loop that gets at most 50 of each type of malware and saves it to a spreadsheet
+    for y in range(9):
         counter = 0
         for x in range(22215):
-            if p_dataset[p_dataset.columns[47]][unique_names[x]] == unique_names[y] and p_dataset[p_dataset.columns[47]][unique_names[x]] != null and counter < 50:
-                for y in range(49):
-                    # Write row here
-                    sheet1.write(a, y, p_dataset[p_dataset.columns[x]][y]) 
-                    a += 1
-                    counter += 1
+            if p_dataset[p_dataset.columns[47]][new_line[x]] == unique_names[y+1] and counter < 50:
+                for z in range(49):
+                    sheet1.write(a, z, str(p_dataset[p_dataset.columns[z]][new_line[x]]))
+                a += 1
+                counter += 1
+            elif(counter > 49):
+                break
 
-    wb.save('perceptron_test_set.csv') 
+    # Resetting Counter so the next loop is not skipped
+    counter = 0
 
-#digest_file()
-make_test_file()
+    print('\nSaving list of clean packets, please wait.')
+
+    # For loop that gets at most 50 clean packets and saves to spreadsheet
+    for x in range(22215):
+        if int(p_dataset[p_dataset.columns[48]][x]) == 0 and counter < 50:
+            for z in range(49):
+                sheet1.write(a, z, str(p_dataset[p_dataset.columns[z]][x])) 
+            a += 1
+            counter += 1
+        elif(counter > 49):
+            break
+
+    # Create new XLS and save values to it
+    wb.save('perceptron_test_set.xls') 
+
+    print('\nFile Created: perceptron_test_set.xls \nConvert to CSV in Excel.\n')
+
+    # CONVERT TO CSV IN EXCEL
+
+digest_file()
+#make_test_file()
