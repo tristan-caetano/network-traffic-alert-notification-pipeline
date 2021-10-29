baf90211b2e48a7bc41d3251e2819aae8b6dd096
Jason Pinto <pintojason@outlook.com>
2021-10-29 17:17:36 -0400

    practice

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -5,13 +5,28 @@
 # https://www.youtube.com/watch?v=Ycp4Si89s5Q&ab_channel=SebastianRaschka
 
 # Libraries
-import matplotlib.pyplot as plt
-import pandas as pd
+import csv
 import torch
-
-import time
 import numpy as np
-from torchvision import datasets
-from torchvision import transforms
-from torch.utils.data import DataLoader
-import torch.nn.functional as F
+import pandas as pd
+
+# Pandas Import CSV
+Data_set = pd.read_csv('perceptron_test_set.csv')
+
+# Converts the data to Pytorch tensor
+Pytorch_data = torch.tensor(Data_set.values)
+# print(Pytorch_data)
+# print(type(Pytorch_data))
+
+data = Pytorch_data[:, :-1]
+# print(data)
+
+target = Pytorch_data[:, :]
+
+# print(target)
+# print(target.shape) # prints: torch.Size([473])
+print(target.unique())
+
+# target_onehot = torch.zeros(target.shape[0], 10)
+# print(target_onehot)
+
