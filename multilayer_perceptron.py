7b7bea37c257efffd55807e3ed3ab60b87457bb1
Jason Pinto <pintojason@outlook.com>
2021-10-28 22:09:55 -0400

    AI-algorithm

---
+++ multilayer_perceptron.py
@@ -0,0 +1,17 @@
+# Multilayer perceptron
+
+# PyTorch Tutorials
+# https://www.youtube.com/watch?v=GIsg-ZUy0MY&ab_channel=freeCodeCamp.org
+# https://www.youtube.com/watch?v=Ycp4Si89s5Q&ab_channel=SebastianRaschka
+
+# Libraries
+import matplotlib.pyplot as plt
+import pandas as pd
+import torch
+
+import time
+import numpy as np
+from torchvision import datasets
+from torchvision import transforms
+from torch.utils.data import DataLoader
+import torch.nn.functional as F
