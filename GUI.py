d4de6171dbffa69c8a1599fef5bf74e78ecb0c0d
Tristan Caetano <tristancostacaetano@gmail.com>
2021-11-07 18:41:59 -0500

    Working GUI with 3 buttons, must be compiles in PS

--- GUI.py
+++ GUI.py
@@ -1,12 +1,17 @@
 import tkinter as tk
+import os
 from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
 
-win= Tk()
+# if os.environ.get('DISPLAY','') == '':
+#     print('no display found. Using :0.0')
+#     os.environ.__setitem__('DISPLAY', ':0.0')
+
+win = tk.Tk()
 
 #resize window
-win.geometry("700x350")
+#win.geometry("700x350")
 
 #this is what happens when you click the import button
 def import_box(event=None):
@@ -30,8 +35,8 @@ ttk.Button(win, text= "Clear Queue", command=clear_queue).pack(pady= 20)
 ttk.Button(win, text= "Analyze", command=start).pack(pady= 20)
 
 #create output textbox
-output = tk.Text(win, width = 25, height = 50)
-output.grid(row = 1, column = 0, columnspan = 50)
+# output = tk.Text(win, width = 25, height = 50)
+# output.grid(row = 1, column = 0, columnspan = 50)
 #to clear the output window -> output.delete(1.0,"") or something like that???
 #to add to the output window -> output.insert("blhablahblahb") i think???
 
