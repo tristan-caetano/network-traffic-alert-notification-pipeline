4832a97ff2bc9a450bd237c4d4e79fcfa27ab783
tristan-caetano <tristancostacaetano@gmail.com>
2021-11-07 19:12:02 -0500

    Window should work on Windows and Linux

--- GUI.py
+++ GUI.py
@@ -4,9 +4,7 @@ from tkinter import *
 from tkinter import filedialog
 from tkinter import ttk
 
-# if os.environ.get('DISPLAY','') == '':
-#     print('no display found. Using :0.0')
-#     os.environ.__setitem__('DISPLAY', ':0.0')
+os.environ.__setitem__('DISPLAY', ':0.0')
 
 win = tk.Tk()
 
