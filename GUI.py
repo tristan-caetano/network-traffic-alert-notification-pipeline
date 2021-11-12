98750e79e4c567ad275048b8d69f78f0b02fb3a0
Tate DeTerra <tatedeterra@gmail.com>
2021-11-12 13:25:22 -0500

    added CSV view and moved output, and some other stuff

--- GUI.py
+++ GUI.py
@@ -8,6 +8,7 @@ from tkinter import ttk
 import os
 import os.path
 import threading
+import csv
 
 win= Tk()
 
@@ -19,21 +20,24 @@ win.title("cool gui for cool boys")
 def import_box(event=None):
    global filename
    filename = filedialog.askopenfilename()
-   print('Added:', filename)
-   lst.append(filename)
-   queue.configure(state='normal')
-   queue.insert(tk.END, filename)
-   queue.insert(tk.END, '\n')
-   queue.see(tk.END)
-   queue.configure(state='disabled')
-   print(lst)
+   filename = os.path.basename(filename)
+   output.configure(state='normal')
+   my_str = filename[-5]
+   if my_str == ".pcap":
+      output.insert(tk.END, 'Selected PCAP: ')
+      output.insert(tk.END, filename)
+      output.insert(tk.END, '\n')
+      output.see(tk.END)
+      output.configure(state='disabled')
+   else:
+      output.insert(tk.END, 'File is not a PCAP\n')
+      output.see(tk.END)
+      output.configure(state='disabled')
 
 def import_box2(event=None):
    global filenameCSV
    filenameCSV = filedialog.askopenfilename()
-   filenameCSV  = os.path.basename(filenameCSV)
-   output.configure(state='normal')
-   output.delete(1.0, END)
+   filenameCSV = os.path.basename(filenameCSV)
 
    # The if-statement and else-statement detects if the file is a CSV or not.
    my_str = filenameCSV[-4:]
@@ -48,14 +52,6 @@ def import_box2(event=None):
       output.see(tk.END)
       output.configure(state='disabled')
 
-# This is what happens when you click the clear button.
-def clear_queue(event=None):
-   print('queue clear event')
-   queue.configure(state='normal')
-   queue.delete(1.0, END)
-   queue.configure(state='disabled')
-   lst.clear()
-
 # This is what happens when you click the GO button.
 def start(event=None):
    print('start conversion')
@@ -65,6 +61,13 @@ def start(event=None):
    output.configure(state='disabled')
    cv.convert(output, filename)
    # Call the conversion script
+   
+#this is what happens when you click the clear button
+def clear_output(event=None):
+   print('output clear event')
+   output.configure(state='normal')
+   output.delete(1.0, END)
+   output.configure(state='disabled')
 
 # This is what happens when you click the CREATE TEST SET
 def create_set(event=None):
@@ -80,27 +83,51 @@ def create_set(event=None):
 
 # Create labels
 Label(win, text= "WORLDS GREATEST GUI").pack(pady= 10)
-Label(win, text= "QUEUE:").place(x=50, y=184)
-Label(win, text= "OUTPUT:").place(x=400, y=40)
+Label(win, text= "OUTPUT:").place(x=50, y=184)
+Label(win, text= "CSV:").place(x=400, y=20)
 
 # Create buttons
 ttk.Button(win, text= "Import PCAP", command=import_box).place(x=50, y=40)
-ttk.Button(win, text= "Clear Queue", command=clear_queue).place(x=50, y=80)
-ttk.Button(win, text= "Convert to CSV", command=start).place(x=50, y=120)
-ttk.Button(win, text= "Import CSV", command=import_box2).place(x=150, y=40)
-ttk.Button(win, text= "Create Test Set", command=create_set).place(x=150, y=80)
-
-# File queue
-queue = Text(win, state = 'disabled', width=40, height=31)
-queue.place(x=50, y=204)
+ttk.Button(win, text= "Convert to CSV", command=start).place(x=50, y=80)
+ttk.Button(win, text= "Clear Output", command=clear_output).place(x=50, y=150)
+ttk.Button(win, text= "Import CSV", command=import_box2).place(x=180, y=40)
+ttk.Button(win, text= "Create Test Set", command=create_set).place(x=180, y=80)
 
 # Output window
-output = Text(win, state = 'disabled', width=90, height=40)
-output.place(x=400, y=60)
+output = Text(win, state = 'disabled', width=40, height=36)
+output.place(x=50, y=204)
 
-# Initialize queue list
-lst = []
+#CSV view
+TableMargin = Frame(win, width=100)
+TableMargin.place(x=400, y=40)
 
+scrollbarx = Scrollbar(TableMargin, orient=HORIZONTAL)
+scrollbary = Scrollbar(TableMargin, orient=VERTICAL)
+tree = ttk.Treeview(TableMargin, columns=("1", "2", "3","4"), height=35, selectmode="extended", yscrollcommand=scrollbary.set, xscrollcommand=scrollbarx.set)
+scrollbary.config(command=tree.yview)
+scrollbary.pack(side=RIGHT, fill=Y)
+scrollbarx.config(command=tree.xview)
+scrollbarx.pack(side=BOTTOM, fill=X)
+tree.heading('1', text="1", anchor=W)
+tree.heading('2', text="2", anchor=W)
+tree.heading('3', text="3", anchor=W)
+tree.heading('4', text="4", anchor=W)
+tree.column('#0', stretch=NO, minwidth=0, width=0)
+tree.column('#1', stretch=NO, minwidth=0, width=40)
+tree.column('#2', stretch=NO, minwidth=0, width=40)
+tree.column('#3', stretch=NO, minwidth=0, width=40)
+tree.column('#3', stretch=NO, minwidth=0, width=40)
+tree.pack()
+#with open(filename) as f:
+#  reader = csv.DictReader(f, delimiter=',')
+#  for row in reader:
+#    1 = row['1']
+#    2 = row['2']
+#    3 = row['3']
+#    4 = row['4']
+#    tree.insert("", 0, values=(1,2,3,4))
+
+#start window
 win.mainloop()
 
 
