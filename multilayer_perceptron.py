6826001112f62da1d290d18166af36845e65be84
Jason Pinto <pintojason@outlook.com>
2021-11-17 21:48:02 -0500

    Simple test MLP

--- multilayer_perceptron.py
+++ multilayer_perceptron.py
@@ -1,32 +1,50 @@
 # Multilayer Perceptron
 
+#  ---------------  Our goal   ---------------
+
+# Develop an Artificial Intelligence / Machine Learning (AI/ML) solution, which monitors real-time network 
+# traffic, classifies groups of packets as either malicious or normal, and notifies the LAN Admin 
+# immediately when consistent, sustained malicious activity has been observed. 
+
 #  ---------------  Libraries  ---------------
-import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
-import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import accuracy_score, confusion_matrix
-
-
-# Prints full rows
-pd.set_option("display.max_rows", None, "display.max_columns", None) 
-
-#  ---------------  Parameters for models  ---------------
-hidden_neurons = 50
-epochs = 10000
-learning_rate = 0.01
-no_change_thresh = epochs
+from sklearn.metrics import accuracy_score
 
 
 #  ---------------  Data set   ---------------
 file = 'perceptron_test_set.csv'
-Data_set = pd.read_csv(file, header=None)
-# Data_set.columns = ['s_length', 's_width', 'p_length', 'p_width', 'class']
-# print(Data_set)
-x = Data_set.iloc[:, 0:4]
-# y = Data_set['class']
-print(x)
+Data_set = pd.read_csv(file)
+Data_set.head()
 
-# Split test data from training data, 80% Training, 20% Test
-# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, shuffle=True, random_state=8)
+
+#  ---------------  Spliting data into Feature   ---------------
+X = Data_set[[Data_set.columns[0], Data_set.columns[1], Data_set.columns[2], Data_set.columns[3], Data_set.columns[4], Data_set.columns[5], Data_set.columns[6], Data_set.columns[7], Data_set.columns[8], Data_set.columns[9], Data_set.columns[10], Data_set.columns[11], Data_set.columns[12], Data_set.columns[13], Data_set.columns[14], Data_set.columns[15], Data_set.columns[16], Data_set.columns[17], Data_set.columns[18], Data_set.columns[19], Data_set.columns[20], Data_set.columns[21], Data_set.columns[22], Data_set.columns[23], Data_set.columns[24], Data_set.columns[25], Data_set.columns[26], Data_set.columns[27], Data_set.columns[28], Data_set.columns[29], Data_set.columns[30], Data_set.columns[31], Data_set.columns[32], Data_set.columns[33], Data_set.columns[34], Data_set.columns[35], Data_set.columns[36], Data_set.columns[37], Data_set.columns[38], Data_set.columns[39], Data_set.columns[40], Data_set.columns[41], Data_set.columns[42], Data_set.columns[43], Data_set.columns[44], Data_set.columns[45], Data_set.columns[46]]]
+y = Data_set[Data_set.columns[47]]
+
+
+
+#  ---------------  Split dataset into training set and test set   ---------------
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)  # 70% training and 30% test
+
+
+#  ---------------  Create model object   ---------------
+clf = MLPClassifier(hidden_layer_sizes=(6,5),
+                    random_state=5,
+                    verbose=True,
+                    learning_rate_init=0.01)
+
+
+#  ---------------  Fit data onto the model   ---------------
+clf.fit(X_train, y_train)
+
+
+#  ---------------  Make prediction   ---------------
+ypred = clf.predict(X_test)
+
+
+#  ---------------  Calcuate accuracy   ---------------
+print('\nOutput:')
+print(accuracy_score(y_test, ypred))
