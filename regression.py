aa1e827db1e4db9b6dae382112da17b28cc171bc
Tristan Caetano <tristancostacaetano@gmail.com>
2022-01-07 17:14:24 -0500

    Binary algorithm

---
+++ regression.py
@@ -0,0 +1,39 @@
+import tensorflow as tf
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.datasets import make_circles
+
+# Make 1000 examples
+n_samples = 1000
+
+# Create circles
+X, y = make_circles(n_samples, 
+                    noise=0.03, 
+                    random_state=42)
+
+circles = pd.DataFrame({"X0":X[:, 0], "X1":X[:, 1], "label":y})
+circles.head()
+
+plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.RdYlBu)
+plt.show()
+
+# Set random seed
+tf.random.set_seed(42)
+
+# 1. Create the model (same as model_1 but with an extra layer)
+model_2 = tf.keras.Sequential([
+  tf.keras.layers.Dense(10000),
+  tf.keras.layers.Dense(1000), # add an extra layer
+  tf.keras.layers.Dense(100),
+  tf.keras.layers.Dense(10)
+])
+
+# 2. Compile the model
+model_2.compile(loss=tf.keras.losses.BinaryCrossentropy(),
+                optimizer=tf.keras.optimizers.Adam(),
+                metrics=['accuracy'])
+
+# 3. Fit the model
+model_2.fit(X, y, epochs=100, verbose=0) # set verbose=0 to make the output print less
+model_2.evaluate(X, y)
