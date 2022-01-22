929c5603a5a3419d7760941742b1a0072222b7fe
Tristan Caetano <tcaetano@localhost.localdomain>
2022-01-21 23:06:51 -0500

    Integrated normalized dataset to AI algorithm/90% accuracy with model

--- regression.py
+++ regression.py
@@ -6,29 +6,18 @@ from sklearn.datasets import make_circles
 
 dataset = "n_p_dataset.csv"
 p_dataset = pd.read_csv(dataset, low_memory=False)
+class_df = p_dataset[p_dataset.columns[16]]
+p_dataset = p_dataset.drop(['1.3'], axis = 1)
 
-# Make 1000 examples
-n_samples = 1000
+print(p_dataset)
 
-# Create circles
-X, y = make_circles(n_samples, 
-                    noise=0.03, 
-                    random_state=42)
-
-print(y)
-
-circles = pd.DataFrame({"X0":X[:, 0], "X1":X[:, 1], "label":y})
-circles.head()
-
-plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.RdYlBu)
-plt.show()
 
 # Set random seed
 tf.random.set_seed(42)
 
 # 1. Create the model (same as model_1 but with an extra layer)
 model_2 = tf.keras.Sequential([
-  tf.keras.layers.Dense(10000),
+  # tf.keras.layers.Dense(10000),
   tf.keras.layers.Dense(1000), # add an extra layer
   tf.keras.layers.Dense(100),
   tf.keras.layers.Dense(10),
@@ -41,5 +30,9 @@ model_2.compile(loss=tf.keras.losses.BinaryCrossentropy(),
                 metrics=['accuracy'])
 
 # 3. Fit the model
-model_2.fit(X, y, epochs=100, verbose=0) # set verbose=0 to make the output print less
-model_2.evaluate(X, y)
+# model_2.fit(X, y, epochs=100, verbose=0) # set verbose=0 to make the output print less
+# model_2.evaluate(X, y)
+
+# 3. Fit the model
+model_2.fit(p_dataset, class_df, epochs=100, verbose=0) # set verbose=0 to make the output print less
+model_2.evaluate(p_dataset, class_df)
